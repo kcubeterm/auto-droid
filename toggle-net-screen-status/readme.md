0818250dbf60a11020dc05812ccd7b3c0dc78567
kcubeterm <kcubeterm@gmail.com>
2022-02-07 18:17:57 +0530

    initial commit

---
+++ toggle-net-screen-status/readme.md
@@ -0,0 +1,8 @@
+# Data toggler on the status of screen. 
+this script could be run in termux as well as your PC. this script assume you are connected with adb. in 
+case you are connected with more than one device. consider executing this script with `./togglenet.sh IP:PORT`
+
+## what it do
+It is more like a power saving strategy, android itself is a beast when comes to saving power. anyway!!. 
+it will gonna turn off mobile data and wifi if screen is off or if screen on it will turn on. thats it. 
+
---
+++ toggle-net-screen-status/togglenet.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash 
+
+# argument 1st could contains device address in case more than one devices is connected
+adb=adb
+if [ $# -eq 1 ];then
+    adb="adb -s ${1}"
+fi
+
+ScreenStatus=$($adb shell dumpsys power | grep 'Display Power' | cut -d'=' -f2)
+
+svc="$adb shell svc"
+while true;
+do 
+    case $ScreenStatus in
+    'OFF') 
+        $svc data disable
+        $svc wifi disable
+        ;;
+    'ON') 
+        $svc data enable
+        $svc wifi enable
+        ;;
+    *) echo 'report issue'
+    esac
+    sleep 3
+done
